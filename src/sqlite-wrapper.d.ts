67d89da3312d034225353e92bb81296a693128bf
Naoyuki Hirayama <naoyuki.hirayama@gmail.com>
2024-07-26 14:45:15 +0900

    fix types

--- src/sqlite-wrapper.d.ts
+++ src/sqlite-wrapper.d.ts
@@ -4,6 +4,6 @@ export class UniversalSQLite {
   constructor(dbPath?: string);
   async init(): Promise<void>;
   async execute(query: string, params: any[] = []): Promise<void>;
-  async query<T extends any[]>(query: string, params: any[] = []): Promise<T>;
+  async query<T = any>(query: string, params: any[] = []): Promise<T[]>;
   async close(): Promise<void>;
 }
